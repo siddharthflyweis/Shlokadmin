a9e1a3873c05728a0659fa9b87b562abc470609d
siddharthflyweis <react05@flyweis.technology>
2024-03-04 12:36:58 +0530

    file structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Login from "./Login/Login";
+
+function App() {
+  return (
+    <div className="text-3xl">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Allfiles.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Allfiles = () => {
+  return (
+    <div>Allfiles</div>
+  )
+}
+
+export default Allfiles
